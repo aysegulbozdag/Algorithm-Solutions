490832634da4cff32672f6affdb6cd3200111498
Ayşegül Bozdağ <aysegulbozdag4@gmail.com>
2023-07-18 21:50:51 +0300

    Found best practice

--- app/src/main/java/com/example/android/algorithmsolutions/ContainsDuplicate.kt
+++ app/src/main/java/com/example/android/algorithmsolutions/ContainsDuplicate.kt
@@ -3,13 +3,14 @@ package com.example.android.algorithmsolutions
 
 fun containsDuplicate(nums: Array<Int>): Boolean {
     val hs = HashSet<Int>()
+
     for (i in nums) {
         hs.add(i)
     }
 
     if (nums.size != hs.size)
         return true
-    
+
     return false
 }
 
