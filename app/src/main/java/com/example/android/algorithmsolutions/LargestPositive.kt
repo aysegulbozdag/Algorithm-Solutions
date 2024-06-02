a2484835c4b0e583884e9be79ba856035251b676
Ayşegül Bozdağ <aysegulbozdag4@gmail.com>
2024-06-02 23:34:38 +0300

    Code ref.

--- app/src/main/java/com/example/android/algorithmsolutions/LargestPositive.kt
+++ app/src/main/java/com/example/android/algorithmsolutions/LargestPositive.kt
@@ -4,20 +4,16 @@ package com.example.android.algorithmsolutions
 fun findMaxK(nums: IntArray): Int {
     val numSet = nums.toSet()
 
-    // 2. Pozitif değerleri filtreleme
     val positives = nums.filter { it > 0 }
 
-    // 3. Büyükten küçüğe sıralama
     val sortedPositives = positives.sortedDescending()
 
-    // 4. Negatif değerlerin varlığını kontrol etme
     for (k in sortedPositives) {
         if (-k in numSet) {
             return k
         }
     }
 
-    // 5. Sonuç döndürme
     return -1
 }
 
