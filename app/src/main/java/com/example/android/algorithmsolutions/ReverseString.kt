e338312949d18e7c4609e35b164874fde89ec071
Ayşegül Bozdağ <aysegulbozdag4@gmail.com>
2024-06-03 12:31:11 +0300

    ReverseString.kt added.

---
+++ app/src/main/java/com/example/android/algorithmsolutions/ReverseString.kt
@@ -0,0 +1,10 @@
+package com.example.android.algorithmsolutions
+
+//https://leetcode.com/problems/reverse-string/description/?envType=daily-question&envId=2024-06-02
+fun reverseString(s: CharArray): Unit {
+    s.reverse()
+}
+
+fun main() {
+    reverseString(charArrayOf('h', 'e', 'l', 'l', 'o'))
+}
